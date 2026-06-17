e4fb56933d76e4311736466f07cbf1b319911ff3
BionaSibichan <bionasibichan@gmail.com>
2026-06-17 10:49:17 +0530

    pattern added

--- pattern.py
+++ pattern.py
@@ -1,4 +1,15 @@
-for r in range(5):
-    for j in range(r+1):
-        print("*",end=" ")
-    print("")
+# for r in range(5):
+#     for j in range(r+1):
+#         print("*",end=" ")
+#     print("")   OR
+
+for r in range(1,6):
+    print("* "*r)
+
+# for r in range(1,6):
+#     print("*"(6-r)*r)
+        
+# Right-aligned star pattern
+
+for r in range(1, 6):
+    print(" " * (6 - r) + "*" * r)
